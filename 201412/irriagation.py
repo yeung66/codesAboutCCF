b295f41370e4e9cc6ca36992e309f2fed485888a
Scott <yeunghl@whu.edu.cn>
2019-03-14 16:57:36 +0800

    commit 201412-4 最优灌溉 最小生成树问题

---
+++ 201412/irriagation.py
@@ -0,0 +1,32 @@
+n,m = list(map(int,input().split()))
+edges = [{}for _ in range(n+1)]
+
+for _ in range(m):
+    u,v,c = list(map(int,input().split()))
+    if v in edges[u]:c=min(edges[u][v],c)
+    edges[u][v]=c
+    edges[v][u]=c
+from math import inf as INF
+visited = [False for _ in range(n+1)]
+dp = [INF for _ in range(n+1)]
+
+from queue import PriorityQueue
+pq = PriorityQueue()
+dp[1]=0
+pq.put((0,1))
+ans,count = 0,0
+while not pq.empty():
+    c,front = pq.get()
+    if visited[front]:continue
+    visited[front]=True
+    ans+=c
+    count+=1
+    for e,cost in edges[front].items():
+        if not visited[e]:
+            dp[e] = min(dp[e],cost)
+            pq.put((dp[e],e))
+    if count==n:break
+
+##print(dp[1:])
+print(ans)
+      
